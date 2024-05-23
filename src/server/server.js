964a8fb9ee39a38d4d3e43262bd1cb204e0d9350
StasAnanas666 <lapshin-5a@yandex.ru>
2024-05-23 13:57:53 +0300

    work edit task

--- src/server/server.js
+++ src/server/server.js
@@ -5,56 +5,63 @@ const db = require("./db");
 const app = express();
 const port = 3001;
 
-app.use(express.json());//использование авто сериализации/десериализации json
-app.use(cors());//обход cors-блокировок
+app.use(express.json()); //использование авто сериализации/десериализации json
+app.use(cors()); //обход cors-блокировок
 
 //получение списка задач
 app.get("/tasks", (req, res) => {
     db.all("select * from tasks", (err, rows) => {
-        if(err) {
+        if (err) {
             //в случае ошибки возвращает статус ошибки сервера и сообщение с описанием ошибки
-            return res.status(500).json({error: err.message});
+            return res.status(500).json({ error: err.message });
         }
         res.json(rows);
-    })
-})
+    });
+});
 
 //добавление задачи
 app.post("/tasks", (req, res) => {
-    const {title, deadline, priority} = req.body;
-    db.run("insert into tasks(title, deadline, priority) values(?,?,?)", [title, deadline, priority], (err) => {
-        if(err) {
-            return res.status(500).json({error: err.message});
+    const { title, deadline, priority } = req.body;
+    db.run(
+        "insert into tasks(title, deadline, priority) values(?,?,?)",
+        [title, deadline, priority],
+        (err) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.json({ id: this.lastID });
         }
-        res.json({id: this.lastID});
-    })
-})
+    );
+});
 
 //изменение задачи
 app.put("/tasks/:id", (req, res) => {
-    const {id} = req.params;
-    console.log(id);
-    const {title, deadline, priority} = req.body;
-    db.run("update tasks set title = ?, deadline = ?, priority = ? where id = ?", [title, deadline, priority, id], (err) => {
-        if(err) {
-            return res.status(500).json({error: err.message});
+    const { id } = req.params;
+    const { title, deadline, priority } = req.body;
+    db.run(
+        "update tasks set title = ?, deadline = ?, priority = ? where id = ?",
+        [title, deadline, priority, id],
+        (err) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            res.json({ id, title, deadline, priority });
         }
-        res.json({id, title, deadline, priority});
-    })
-})
+    );
+});
 
 //удаление задачи
 app.delete("/tasks/:id", (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     db.run("delete from tasks where id=?", id, (err) => {
-        if(err) {
-            return res.status(500).json({error: err.message});
+        if (err) {
+            return res.status(500).json({ error: err.message });
         }
-        res.json({deleted: this.changes});
-    })
-})
+        res.json({ deleted: this.changes });
+    });
+});
 
 //запуск сервера
 app.listen(port, () => {
     console.log(`Сервер запущен по адресу: http://localhost:${port}`);
-})
+});
